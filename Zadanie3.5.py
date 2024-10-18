71387d52a9713a903fd08938d4036a82c7b49c75
Pawel Halka <arktykahalka@gmail.com>
2024-10-18 18:50:58 +0200

    Add capital letter for shops

--- Zadanie3.5.py
+++ Zadanie3.5.py
@@ -2,3 +2,5 @@ shopping_dict = {
     'piekarnia':('chleb', 'bulki', 'paczek'),
     'warzywniak':('marchew', 'seler', 'rukola')
 }
+for shop, products in shopping_dict.items():
+    print('Ide do {}, kupuje tu nastepujace rzeczy: {}'.format(shop.capitalize(),capitalize_products))
